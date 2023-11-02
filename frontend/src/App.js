d8bc366ef33287d83f210976a8da8fd091455839
Juan Pablo Lezcano <jplezcano@Juans-MacBook-Air.local>
2023-11-01 23:14:39 -0700

    Hay comunicacion entre back y front.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import axios from "axios";
+
 import Register from "./components/Register";
+
 function App() {
   return (
     <div>
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,27 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    loginEmail: "",
+    loginPassword: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // Lógica para el registro del usuario
+    axios
+      .post("http://localhost:3001/api/users/register", {
+        name: formData.name,
+        last_name: formData.lastName,
+        email: formData.email,
+        password: formData.password,
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("Registro exitoso");
+        }
+      })
+      .catch((error) => {
+        console.error("Error al registrar usuario:", error);
+      });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Lógica para el inicio de sesión del usuario
+    axios
+      .post("http://localhost:3001/api/users/login", {
+        email: formData.loginEmail,
+        password: formData.loginPassword,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Inicio de sesión exitoso");
+        }
+      })
+      .catch((error) => {
+        console.error("Error al iniciar sesión:", error);
+      });
+  };
+
   return (
     <div className="register">
       <div className="background-image"></div>
       <div className="form-container">
         <form className="register-form">
-          <h2>Register</h2>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
           <div>
-            <button>Register</button>
+            <button onClick={handleRegister}>Register</button>
           </div>
         </form>
 
         <form className="login-form">
-          <h2>Login</h2>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input
+            type="email"
+            name="loginEmail"
+            placeholder="Email"
+            value={formData.loginEmail}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            name="loginPassword"
+            placeholder="Password"
+            value={formData.loginPassword}
+            onChange={handleInputChange}
+          />
           <div>
-            <button>Login</button>
+            <button onClick={handleLogin}>Login</button>
           </div>
         </form>
       </div>
